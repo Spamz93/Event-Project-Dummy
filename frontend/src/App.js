9c3a519a6b40d529903c82a2a8e1ed8964707c82
Spamz93 <131263613+Spamz93@users.noreply.github.com>
2025-02-14 13:56:17 +0200

    fixed auth + UI based on token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,35 @@
-// Challenge / Exercise
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// DONE
-
-
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// DONE
-
-
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// DONE
-
-// 4. Add properly working links to the MainNavigation
-// DONE
-
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// DONE
-
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// DONE
-
-// 7. Output the ID of the selected event on the EventDetailPage
-// DONE
-
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-// DONE
-
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
-import HomePage from './pages/Home';
-import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventDetailPage, { 
-    loader as eventDetailLoader,
-    action as deleteEventAction
-  } from './pages/EventDetail';
-import NewEventPage, {action as newEventAction} from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
-import RootLayout from './pages/Root';
-import EventsRootLayout from './pages/EventsRoot';
 import ErrorPage from './pages/Error';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from './pages/EventDetail';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
+import EventsRootLayout from './pages/EventsRoot';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+import RootLayout from './pages/Root';
+import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+import AuthenticationPage,{action as authAction} from './pages/Authentication';
+import {action as logoutAction} from './pages/Logout'
+import { tokenLoader } from './util/auth';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: 'events', element: <EventsRootLayout />, children: [
+        path: 'events',
+        element: <EventsRootLayout />,
+        children: [
           {
             index: true,
             element: <EventsPage />,
@@ -73,24 +43,42 @@ const router = createBrowserRouter([
               {
                 index: true,
                 element: <EventDetailPage />,
-                action: deleteEventAction
+                action: deleteEventAction,
               },
-              { path: 'edit', element: <EditEventPage /> },
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
             ],
           },
-
-          { path: 'new', element: <NewEventPage />, action: newEventAction },
-          
-        ]
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+        ],
       },
-
+      {
+        path: 'auth',
+        element: <AuthenticationPage />,
+        action: authAction,
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
+      {
+        path: 'logout',
+        action: logoutAction,
+      }
     ],
   },
-
 ]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
 export default App;
